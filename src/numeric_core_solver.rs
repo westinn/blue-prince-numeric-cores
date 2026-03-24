8c1a54f9687becfcb46ab2a0fda23bcacfa1dbf6
Nick Westin <14435884+westinn@users.noreply.github.com>
2026-03-24 17:27:33 -0400

    Further WIP; updated input functions to handle state machine failures a little better

--- src/numeric_core_solver.rs
+++ src/numeric_core_solver.rs
@@ -1,15 +1,14 @@
 use num_traits::{FromPrimitive, Num, ToPrimitive};
 use std::{
-    fmt::{Debug, Display, format},
-    fs,
-    str::pattern::Pattern,
-    u32,
+    fmt::{Debug, Display},
+    fs, u32,
 };
 
 // These are the states that we can be in while processing a Number that could be a NumericCore
 // a valid NumericCore result: a whole number with 3 or less digits, >0 and <1000
 // a Processable value: a whole number with 4 or more digits, aka >1000
 // an Invalid result: a non-whole number, or a negative number
+
 #[derive(Debug)]
 enum NumericCoreState {
     NumericCore(NumericCoreValue),
@@ -22,48 +21,49 @@ impl NumericCoreState {
     where
         T: Num + PartialOrd + FromPrimitive + ToPrimitive + Copy + Display,
     {
-        // TODO: Converted these to Result/Err, since returning None would be a faulty state
-        let float_value = input_value.to_f64().ok_or(format!(
-            "Unable to convert input to f64 value: {input_value}"
-        ))?;
-        let int_value = input_value.to_u32().ok_or(format!(
-            "Unable to convert input to u32 value: {input_value}"
-        ))?;
+        // @TODO: Converted these to Result/Err, since returning None would be a faulty state
+        let float_value = input_value.to_f64().ok_or_else(|| {
+            format!("Unable to convert input to f64 value for NumericCoreState creation: {input_value} !=> f64")
+        })?;
+        let int_value = input_value.to_u32().ok_or_else(|| {
+            format!("Unable to convert input to u32 value for NumericCoreState creation: {input_value} !=> u32")
+        })?;
 
         // if non-whole number or negative, it is not valid
         if float_value.fract() != 0.0 || int_value <= 0 {
             return Ok(NumericCoreState::Invalid);
         }
 
+        // bounds checking for final integer value
         match int_value {
             1..1000 => Ok(NumericCoreState::NumericCore(NumericCoreValue(int_value))),
             1000.. => Ok(NumericCoreState::Processable(ProcessableValue(int_value))),
-            _ => Err(format!(
-                "Unable to use input value to create NumericCoreState: {input_value} as {int_value}"
-            )),
+            _ => Ok(NumericCoreState::Invalid),
         }
     }
 }
 
+// trait ValidState {
+//     fn get_numeric_core(self) -> Option<NumericCoreState>;
+// }
+
 #[derive(Debug)]
 struct NumericCoreValue(u32);
 
-impl NumericCoreValue {
-    pub fn get_result(self) -> u32 {
-        self.0
-    }
-}
+// impl ValidState for NumericCoreValue {
+//     fn get_numeric_core(self) -> Option<NumericCoreState> {
+//         self
+//     }
+// }
 
 #[derive(Debug)]
 struct ProcessableValue(u32);
 
-impl ProcessableValue {
-    fn get_numeric_core(self) -> NumericCoreState {
-        // find a value that may or may not be the final result,
-        // thus we return the next State given that value
-        todo!();
-    }
-}
+// impl ProcessableValue {
+//     fn get_numeric_core(self) -> Option<NumericCoreState> {
+//         todo!()
+//     }
+// }
 
 /*
 take cypher as matrix of strings
@@ -83,7 +83,7 @@ for each NumericCoreIteration,
 #[derive(Debug)]
 pub struct NumericCoreSolver {
     intial_cypher_matrix: Vec<Vec<String>>,
-    // numeric_cypher_matrix: Vec<Vec<u32>>
+    numeric_cypher_matrix: Vec<Vec<NumericCoreState>>,
 }
 
 impl NumericCoreSolver {
@@ -99,10 +99,10 @@ impl NumericCoreSolver {
         if let Some(faulty_word_input) = file_contents
             .trim()
             .split_ascii_whitespace()
-            .find(|word| word.chars().any(|c| !c.is_ascii_alphabetic()))
+            .find(|word| word.chars().any(|c: char| !c.is_ascii_alphabetic()))
         {
             return Err(format!(
-                "Unable to parse word as cypher string: {faulty_word_input}"
+                "Unable to parse word from cypher as ascii string: {faulty_word_input}"
             ));
         }
 
@@ -115,32 +115,48 @@ impl NumericCoreSolver {
                     .collect()
             })
             .collect();
+
+        let numeric_cypher_matrix = Self::convert_to_numeric_cypher(&cypher_from_input);
+
         Ok(NumericCoreSolver {
             intial_cypher_matrix: cypher_from_input,
+            numeric_cypher_matrix: numeric_cypher_matrix,
         })
     }
 
-    pub fn get_initial_cypher(&self) -> &[Vec<String>] {
-        &self.intial_cypher_matrix
-    }
-
-    fn convert_to_numeric_cypher(&self) -> Vec<Vec<NumericCoreState>> {
-        let initial_matrix: &[Vec<String>] = self.get_initial_cypher();
-        let converted_to_numeric_cypher: Vec<Vec<u32>> = initial_matrix
+    fn convert_to_numeric_cypher(
+        initial_string_cypher: &[Vec<String>],
+    ) -> Vec<Vec<NumericCoreState>> {
+        let converted_to_numeric_cypher: Vec<Vec<NumericCoreState>> = initial_string_cypher
             .iter()
             .map(|line: &Vec<String>| {
                 line.iter()
-                    .map(|word| Self::convert_word_to_number(word))
+                    .map(|word: &String| {
+                        let word_as_number: u32 = Self::convert_word_to_number(word);
+                        let word_as_core_state: NumericCoreState =
+                            NumericCoreState::new(word_as_number)
+                                .expect(&format!("Unable to create NumericCoreState from number derived from initial cypher word: {word} as {word_as_number}"));
+                        word_as_core_state
+                        // @TODO: Do I exit here if initial input contains something that turns into a NumericCoreState::Invalid?
+                        //        I would want that to only happen after processing value further in the state machine.
+                        //        But maybe faulty input is fine since later values are not dependant on earlier values
+                    })
                     .collect()
             })
             .collect();
+        converted_to_numeric_cypher
 
-        // self.initial_cypher_matrix -> a numerical matrix
-        todo!();
+        // @TODO: related to above todo, in case we do want to error out of an initial bad input state
+        // let any_invalid_inputs = converted_to_numeric_cypher.iter().any(|vec_of_state| {
+        //     vec_of_state
+        //         .iter()
+        //         .any(|state| matches!(state, NumericCoreState::Invalid))
+        // });
     }
 
     fn convert_word_to_number(word: &str) -> u32 {
         // will never have a non ascii alphabetical character due to solver constructor check
+        // thus will never be out of bounds
         let word_as_number_string: String = word
             .chars()
             .map(|c| ((c.to_ascii_lowercase() as u32) - ('a' as u32) + 1).to_string())
@@ -153,5 +169,13 @@ impl NumericCoreSolver {
     fn solve_cypher(&self) -> Vec<Vec<u32>> {
         todo!();
         let current_cypher = self;
+    }
+
+    pub fn get_initial_cypher(&self) -> &[Vec<String>] {
+        &self.intial_cypher_matrix
+    }
+
+    pub fn get_numeric_cypher(&self) -> &[Vec<NumericCoreState>] {
+        &self.numeric_cypher_matrix
     }
 }
