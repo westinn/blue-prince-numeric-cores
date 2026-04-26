18e0927a497d69e497983e1839c25240c9388d15
Nick Westin <14435884+westinn@users.noreply.github.com>
2026-04-25 21:02:45 -0400

    Added basic gui

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,2 @@
 pub mod numeric_core_solver;
-
-use wasm_bindgen::prelude::*;
-
-use crate::numeric_core_solver::NumericCoreSolver;
-
-#[wasm_bindgen]
-pub fn process_input_from_web(input_content: &str) -> String {
-    let solver = NumericCoreSolver::new(input_content);
-    let string_output = format!("{}", solver);
-    string_output
-}
+pub mod web_api;
